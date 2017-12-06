24422b7807dc1962def2567420b5d790c21bacb2
filipeportes <s3nh4@g1thub>
2017-12-06 17:52:53 +0100

    -- i_introduction 0 to 4

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaCode1.java
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaCode1.java
@@ -4,20 +4,12 @@ import util.JavaCode;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class JavaCode1 extends JavaCode {
     public String task1(Collection<Integer> collection) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        Iterator<Integer> iterator = collection.iterator();
-        while (iterator.hasNext()) {
-            Integer element = iterator.next();
-            sb.append(element);
-            if (iterator.hasNext()) {
-                sb.append(", ");
-            }
-        }
-        sb.append("}");
-        return sb.toString();
+        return collection.stream()
+            .map(i -> i.toString())
+            .collect(Collectors.joining(", ", "{", "}"));
     }
 }
