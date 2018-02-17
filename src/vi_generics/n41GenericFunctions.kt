eab24eccdfceeb5465e36774764e4bc6ce6899ff
filipeportes <s3nh4@g1thub>
2018-02-17 12:11:16 +0100

    vi_generics

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -1,7 +1,6 @@
 package vi_generics
 
 import util.TODO
-import java.util.*
 
 fun task41(): Nothing = TODO(
     """
@@ -20,11 +19,21 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
 }
+
+fun <T, C : MutableCollection<in T>> Collection<T>.partitionTo(coll1: C, coll2: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (element in this) {
+        if (predicate(element)) {
+            coll1.add(element)
+        } else {
+            coll2.add(element)
+        }
+    }
+    return Pair<C, C>(coll1, coll2)
+}
+
